bd6f67b7a1cf6085264707227120a1d34f38fcd9
sebai <sebai@DESKTOP-G04B92L>
2017-10-04 20:40:02 -0300

    Clase persona

---
+++ src/Clases/Persona.java
@@ -0,0 +1,34 @@
+package Clases;
+
+public class Persona {
+    private String nombre;
+    private int id;
+
+    public Persona(String nombre, int id) {
+        this.nombre = nombre;
+        this.id = id;
+    }
+
+    public Persona() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String toString(){
+        return "id: "+getId()+"\n nombre: "+getNombre();
+    }
+}
